2439d179454062b6654f3a92b7bb40617d71efc1
Abdirahman Abdi <abdirahmanm2022@gmail.com>
2025-11-23 17:00:37 +0300

    third commit commit

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,12 +1,15 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
 
     for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
+        debugger;
     }
 
     if (quantity >= 10) {
         totalPrice *= 0.9; // apply 10% discount
+        debugger;
     }
 
     return totalPrice;
